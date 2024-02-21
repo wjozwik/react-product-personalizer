88ba3cb0c6927ebe9e5f7eff28deed719cc70384
Wojciech Jozwik <wj.telefon@gmail.com>
2024-02-21 01:20:43 +0100

    Fix OptionSize component

--- src/components/OptionSize/OptionSize.js
+++ src/components/OptionSize/OptionSize.js
@@ -1,5 +1,6 @@
 import styles from '../ProductForm/ProductForm.module.scss'
 import PropTypes from 'prop-types';
+import clsx from 'clsx';
 
 const OptionSize = props => {
   return (
@@ -7,7 +8,7 @@ const OptionSize = props => {
       <h3 className={styles.optionLabel}>Sizes</h3>
       <ul className={styles.choices}>
       {props.sizes.map(size =>
-          <li key={size.name}><button type="button" className={size.name === props.currentSize && styles.active} onClick={() => props.setCurrentSize(size.name)} >{size.name}</button></li>
+          <li key={size.name}><button type="button" className={clsx(size.name === props.currentSize && styles.active)} onClick={() => props.setCurrentSize(size.name)} >{size.name}</button></li>
           )}
       </ul>
     </div>
